2f7ed8ae25964bd42314bbc2448bb7b41bee3fcf
Marcin Zyla <marcin.zyla0@gmail.com>
2017-08-27 12:42:42 +0200

    [F] Add basic command line arguments

--- bf.py
+++ bf.py
@@ -1,10 +1,25 @@
-
 from Core import input
 from Core import output
 from Core import source
 from Core import interpreter
+from Core import version
+
+#argument parsing
+import argparse
+from argparse import RawTextHelpFormatter
+
+parser = argparse.ArgumentParser(description="Commandline Brainfuck interpreter\nhttps://github.com/demozylak/Brainfuck-interpreter-python", formatter_class=argparse.RawTextHelpFormatter)
+
+parser.add_argument("input_file", type=str, help="source file to be interpreted")
+parser.add_argument('--version', action='version', version='%(prog)s {version}'.format(version=version.__version__))
+
+args = parser.parse_args()
 
 
+s = source.FileSource(args.input_file)
+i = input.StdinInput()
+o = output.StdinOutput()
 
-interpreter.Interpreter(source.FileSource('helloworld.b'), input.StdinInput(), output.StdinOutput()).run()
+
+interpreter.Interpreter(s, i, o).run()
 
