59f8cf6b2489b6649b7dad159f37584888e94c90
Marcin Zyla <marcin.zyla0@gmail.com>
2017-09-07 16:05:12 +0200

    [F] Add -r command line option

--- bf.py
+++ bf.py
@@ -6,17 +6,28 @@ from Core import version
 import argparse
 from argparse import RawTextHelpFormatter
 
-parser = argparse.ArgumentParser(description="Commandline Brainfuck interpreter\nhttps://github.com/demozylak/Brainfuck-interpreter-python", formatter_class=argparse.RawTextHelpFormatter)
+parser = argparse.ArgumentParser(description="Commandline Brainfuck interpreter", epilog="https://github.com/demozylak/Brainfuck-interpreter-python", formatter_class=argparse.RawTextHelpFormatter)
 
-parser.add_argument("source_file", type=argparse.FileType('r'), help="source file to be interpreted")
+parser.add_argument("file", type=argparse.FileType('r'), help="brainfuck source file to be interpreted")
 parser.add_argument('--version', action='version', version='%(prog)s {version}'.format(version=version.__version__))
+parser.add_argument('-r', '--read-from', dest='input_file', metavar='input_file', type=argparse.FileType('r'), help="input file for brainfuck program") 
 
 args = parser.parse_args()
 
+#Arguments interpreting
+#Source
+s = source.FileSource(args.file)
 
-s = source.FileSource(args.source_file)
-i = input.StdinInput()
+#Input
+if(args.input_file != None):
+    i = input.StringInput(args.input_file.read())
+else:
+    i = input.StdinInput()
+
+#Output
 o = output.StdinOutput()
+
+#Memory
 m = memory.Memory()
 
 interpreter.Interpreter(s, i, o, m).run()
