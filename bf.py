971fa1a542f74f544d513f398232754cd4459e32
Marcin Zyla <marcin.zyla0@gmail.com>
2017-09-07 16:12:04 +0200

    [F] Add -i cmdline option

--- bf.py
+++ bf.py
@@ -8,9 +8,10 @@ from argparse import RawTextHelpFormatter
 
 parser = argparse.ArgumentParser(description="Commandline Brainfuck interpreter", epilog="https://github.com/demozylak/Brainfuck-interpreter-python", formatter_class=argparse.RawTextHelpFormatter)
 
-parser.add_argument("file", type=argparse.FileType('r'), help="brainfuck source file to be interpreted")
+parser.add_argument("file", metavar='FILE', type=argparse.FileType('r'), help="brainfuck source file to be interpreted")
 parser.add_argument('--version', action='version', version='%(prog)s {version}'.format(version=version.__version__))
-parser.add_argument('-r', '--read-from', dest='input_file', metavar='input_file', type=argparse.FileType('r'), help="input file for brainfuck program") 
+parser.add_argument('-r', '--read-from', dest='input_file', metavar='INPUT_FILE', type=argparse.FileType('r'), help="input file for brainfuck program") 
+parser.add_argument('-i', '--input', dest='input_string', metavar='INPUT', help="input string for brainfuck program") 
 
 args = parser.parse_args()
 
@@ -21,6 +22,8 @@ s = source.FileSource(args.file)
 #Input
 if(args.input_file != None):
     i = input.StringInput(args.input_file.read())
+elif(args.input_string != None):
+    i = input.StringInput(args.input_string)
 else:
     i = input.StdinInput()
 
