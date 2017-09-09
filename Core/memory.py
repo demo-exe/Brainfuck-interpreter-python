a99bb9fcea0df47482311042f69a17465b0df1ef
Marcin Zyla <marcin.zyla0@gmail.com>
2017-09-09 16:51:56 +0200

    [F] Add memory size and wrap features

--- Core/memory.py
+++ Core/memory.py
@@ -1,19 +1,56 @@
+class OutOfMemoryException(Exception):
+    def __init__(self, mem_index=None):
+        if(mem_index == None):
+            super().__init__()
+        else:
+            super().__init__('Error at index : %i' % mem_index)
+
+
+
 class Memory:
-    def __init__(self):
-       self.mem = []
-       for i in range(30000):
-           self.mem.append(0)
-       self.position = 0
+    def __init__(self, size=30000, 
+                       wrap=True, maxint=256):
+        self.mem = [0]
+        if(size == -1):
+            self.dynamic = True
+            self.minpos = 0
+            self.maxpos = 0
+        else:
+            self.dynamic = False
+            self.minpos = 0
+            self.maxpos = size
+            if(size > 0):
+                for x in range(size):
+                    self.mem.append(0)
+        self.wrap = wrap
+        self.maxint = maxint
+        self.position = 0
+
     def Increase(self):
-        self.mem[self.position] += 1
+        if(self.wrap is True and self.mem[self.position] + 1 > self.maxint):
+            self.mem[self.position] = 0
+        else:
+            self.mem[self.position] += 1
 
     def Decrease(self):
-        self.mem[self.position] -= 1
+        if(self.wrap is True and self.mem[self.position] - 1 < 0):
+            self.mem[self.position] = self.maxint
+        else:
+            self.mem[self.position] -= 1
 
     def MoveRight(self):
+        if(self.dynamic is True):
+            if(self.position+1 > self.maxpos):
+                self.maxpos += 1
+                self.mem.append(0)
+        else:
+            if(self.position+1 > self.maxpos):
+                raise OutOfMemoryException(self.position + 1)
         self.position += 1
 
     def MoveLeft(self):
+        if(self.position - 1 < self.minpos):
+            raise OutOfMemoryException(self.position - 1)
         self.position -= 1
 
     def GetCell(self):
--- bf.py
+++ bf.py
@@ -6,12 +6,23 @@ from Core import version
 import argparse
 from argparse import RawTextHelpFormatter
 
-parser = argparse.ArgumentParser(description="Commandline Brainfuck interpreter", epilog="https://github.com/demozylak/Brainfuck-interpreter-python", formatter_class=argparse.RawTextHelpFormatter)
+parser = argparse.ArgumentParser(description="Commandline Brainfuck interpreter", 
+            epilog="https://github.com/demozylak/Brainfuck-interpreter-python", 
+            formatter_class=argparse.RawTextHelpFormatter)
 
-parser.add_argument("file", metavar='FILE', type=argparse.FileType('r'), help="brainfuck source file to be interpreted")
-parser.add_argument('--version', action='version', version='%(prog)s {version}'.format(version=version.__version__))
-parser.add_argument('-r', '--read-from', dest='input_file', metavar='INPUT_FILE', type=argparse.FileType('r'), help="input file for brainfuck program") 
-parser.add_argument('-i', '--input', dest='input_string', metavar='INPUT', help="input string for brainfuck program") 
+parser.add_argument("file", metavar='FILE', type=argparse.FileType('r'), 
+                        help="brainfuck source file to be interpreted")
+parser.add_argument('--version', action='version', 
+                        version='%(prog)s {version}'.format(version=version.__version__))
+parser.add_argument('-r', '--read-from', dest='input_file', metavar='INPUT_FILE', 
+                        type=argparse.FileType('r'), 
+                        help="input file for brainfuck program") 
+parser.add_argument('-i', '--input', dest='input_string', metavar='INPUT',
+                        help="input string for brainfuck program") 
+parser.add_argument('-w', '--wrap', type=int, dest='wrap', metavar='MAXINT',
+                        help="Wrap memory at specified size\nWithout flag wraps at 256\nSpecify -1 for no wrapping") 
+parser.add_argument('-s', '--size', type=int, dest='size', metavar='SIZE',
+                        help="Specify max memory size\nWithout argument memory is dynamic ( limited only by machine RAM )") 
 
 args = parser.parse_args()
 
@@ -31,7 +42,21 @@ else:
 o = output.StdinOutput()
 
 #Memory
-m = memory.Memory()
+if(args.wrap != None):
+    if(args.wrap == -1):
+        mem_wrap = False
+    else:
+        mem_wrap = True
+        mem_maxint = args.wrap
+else:
+    mem_wrap = True
+    mem_maxint = 256
+
+if(args.size == None):
+    mem_size = -1
+else:
+    mem_size = args.size
+m = memory.Memory(size=mem_size, wrap=mem_wrap, maxint=mem_maxint)
 
 interpreter.Interpreter(s, i, o, m).run()
 
