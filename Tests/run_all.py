16f2aa808c95758466460d9637183307d29ce375
Marcin Zyla <marcin.zyla0@gmail.com>
2017-09-16 19:48:07 +0200

    [I] Tests can now be run from any directory

--- Tests/run_all.py
+++ Tests/run_all.py
@@ -1,7 +1,10 @@
 import unittest
+import os 
+
 
 def load_tests(loader, standard_tests, pattern):
-    return unittest.TestLoader().discover('.')
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    return unittest.TestLoader().discover(dir_path)
 
 if __name__ == '__main__':
     unittest.main(verbosity = 3)
